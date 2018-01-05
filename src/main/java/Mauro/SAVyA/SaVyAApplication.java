137d9292433adbc2db5d2cf0babaf394d2090a17
unknown <maurocimino@hotmail.com>
2018-01-04 21:07:31 -0300

    initial commit

---
+++ src/main/java/Mauro/SAVyA/SaVyAApplication.java
@@ -0,0 +1,12 @@
+package Mauro.SAVyA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaVyAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaVyAApplication.class, args);
+	}
+}
